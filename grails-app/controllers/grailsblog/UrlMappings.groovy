3c973242e8d1dee9d30f75b26d82aea0da74b900
Cooper Legeza <cooperlegeza@gmail.com>
2016-11-28 16:53:10 -0500

    Changing URLMappings

--- grails-app/controllers/grailsblog/UrlMappings.groovy
+++ grails-app/controllers/grailsblog/UrlMappings.groovy
@@ -9,7 +9,7 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
+        "/"(controller: "blogHome", view: "blogHome")
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
