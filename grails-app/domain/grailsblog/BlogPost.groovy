162f5c6ba4a346ea4265538201a2fdaa35505a94
Cooper Legeza <cooperlegeza@gmail.com>
2016-12-05 11:00:09 -0500

    Changed front page to display not all text of longer posts

--- grails-app/domain/grailsblog/BlogPost.groovy
+++ grails-app/domain/grailsblog/BlogPost.groovy
@@ -1,11 +1,22 @@
 package grailsblog
 
 class BlogPost {
+
+	int FRONT_PAGE_POST_LENGTH_LIMIT = 200
 	
 	String title
 	Date dateCreated
 	String author
 	String text
+
+	def textForFrontPage (){
+
+		String frontPageText = text.take(FRONT_PAGE_POST_LENGTH_LIMIT)
+		if(frontPageText.length() == 200){
+			frontPageText = frontPageText + "..."
+		}
+		return frontPageText
+	}
 
 	static hasMany = [comments:Comment]
     static constraints = {
